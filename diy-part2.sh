244ebce66d09e83539bde77d08544bf655534393
yy992266 <yy992266@gmail.com>
2022-05-02 15:36:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/xiaomi-nrv3/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk2-aes/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/encryption/a\set wireless.default_radio${devidx}.key=992266yy' package/kernel/mac80211/files/lib/wifi/mac80211.sh
